d04bc98a2ec94cf84c5d902c0a37e83ef8792815
luciano cabello <lucianocabello36@gmail.com>
2022-07-28 01:46:33 -0300

    Tarea Finalizada

--- main.js
+++ main.js
@@ -1,70 +1,100 @@
 
-listadetareas.addEventListener("submit", ()=>{
-    AgregarTarea();
-})
+let tareasNuevas
 
+let contador = 0
 
+// Obtenemos el id de la lista
+let lista = document.getElementById("lista")
 
-let input = document.getElementById("tareas");
-let contador = 0;
-let tareasDelDia = [];
+// Ver si hay tareas antiguas o si el array está vacío
+let tareasViejas = JSON.parse(localStorage.getItem("tareas")) || []
+console.log(tareasViejas)
 
-
-let AgregarTarea = function(){
-    contador++;
-    
-    let tarea_agregada = input.value;
-    
-    if(tarea_agregada == ""){
-        
-        return alert("Tenes que introducir texto")
-    }
-    input.value = ``;
-    
-    
-    // LocalStorage
-    
-    tareasDelDia.push(tarea_agregada);
-    localStorage.setItem("tareasDelDia", JSON.stringify(tareasDelDia))
-    let tareasParseadas = JSON.parse(localStorage.getItem("tareasDelDia"))
-
-    
-    
-    lista.innerHTML += 
-    `
-        <div class="contenedor-tareas" id="${contador}">
-        <input type="checkbox" name="" id="checkbox">
-        <label for="">${tarea_agregada}</label>
-        <img src="imagenes/borrar.png" alt="" class="borrar" id="Borrar">
-        </div>
-        </div>
-        `
-        actualizacion();
-        
+// Si el array viejo tiene items al momento de cargar la página entonces mostramos las tareas
+if (tareasViejas.length > 0) {
+	mostrarTareas()
 }
 
+// Boton para agregar las tareas
+let boton = document.getElementById("boton")
+boton.addEventListener("click", (e) => {
+	e.preventDefault()
+	agregarTareas()
+})
 
+function agregarTareas() {
+	// Obtener la tarea nueva
+	tareasNuevas = document.getElementById("tareas")
+	console.log(tareasNuevas.value)
 
+	// Guardar la tarea nueva en el array
+	tareasViejas.push(tareasNuevas.value)
+	console.log(tareasViejas)
 
+	tareasNuevas.value = ""
+
+	// Guardar el nuevo array en el localStorage
+	localStorage.setItem("tareas", JSON.stringify(tareasViejas))
+
+	mostrarTareas()
+}
+
+function mostrarTareas() {
+	// Vaciamos el placeholder
+	lista.innerHTML = ""
+
+	// Mostramos nueva lista por cada tarea, usamos reverse para mostrar la ultima tarea primero
+	tareasViejas.reverse().forEach((tarea) => {
+		lista.innerHTML += `
+        <div class="contenedor-tareas boton-submit" id="${contador}">
+        <input type="checkbox" name="" class="form-check-input mt-0" id="checkbox">
+        <label for="">${tarea}</label>
+        <img src="imagenes/borrar.png" alt="" class="borrar" id="Borrar">
+        </div>
+    `
+	})
+}
+
+// Tareas Pendientes y Completadas
 let actualizacion = () =>{
+	// Llamaremos a todos los div que seran las Tareas a realizar en la lista
     let element = lista.querySelectorAll(`div`);
+	// Llamaremos todos los Checkbox checkeados en la lista
     let checkbox = lista.querySelectorAll(`input[type="checkbox"]:checked`);
+	// Pinteremos en el div llamado Stats, las tareas pendientes y tareas a realizar
     stats.innerHTML = `<p>Tareas Pendientes: ${element.length} Completadas: ${checkbox.length}</p>`;
 
 }
 
+// Agregamos un evento a la lista para saber donde hacemos click
 lista.addEventListener("click", (e) =>{
-if (e.srcElement.nodeName == "INPUT") {
-    actualizacion();
-} 
-else if (e.srcElement.nodeName == "IMG"){
-    console.log(e.srcElement.parentNode.id);
-    borrarTarea(e.srcElement.parentNode.id)
-}   
-});
+	//Si hacemos un click en cualquier INPUT de la lista se actualizaran los Stats
+	if (e.srcElement.nodeName == "INPUT") {
+		actualizacion();
+	} 
+	// si hacemos click en cualquier IMG de la lista se ejecutara la funcion borrar tarea con los parametros id
+	else if (e.srcElement.nodeName == "IMG"){
+		console.log(e.srcElement.parentNode.id);
+		borrarTarea(e.srcElement.parentNode.id)
+	}   
+	});
 
-let borrarTarea = (id) =>{
-let tareaABorrar =  document.getElementById(id);
-lista.removeChild(tareaABorrar);
-actualizacion();
+	let borrarTarea = (id) =>{
+		let tareaABorrar =  document.getElementById(id);
+		lista.removeChild(tareaABorrar);
+		actualizacion();
+		}
+
+
+		// Vaciar Lista
+
+		let botonReset = document.getElementById("reset");
+
+botonReset.addEventListener("click", vaciarLista);
+// Cuando se apriete vaciar lista, se limpiara el localstorage y se recargara la pagina
+function vaciarLista (e){
+	e.preventDefault
+	localStorage.clear(tareas);
+	location.reload();
+
 }
